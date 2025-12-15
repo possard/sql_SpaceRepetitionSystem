d9a303932c906f5021ac65c080ca6d307e5bc451
Pascal OSSARD <possard@gmail.com>
2025-12-15 12:09:19 +0100

    add interpreteur SQL

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import duckdb
+
+
+st.write("Interpréteur SQL + Examples de présentation:")
+data = {"a": [1,2,3],"b": [4,5,6],"c": [7,8,9]}
+df = pd.DataFrame(data)
+
+tab0, tab1, tab2, tab3 = st.tabs(["SQL","Chats","Chiens","Hiboux"])
+
+with tab0:
+    st.write("Interpréteur SQL")
+    sql_query = st.text_area(label="Entrez votre requête SQL:")
+    st.write(f"Vous avez entré la requete suivante: {sql_query}")
+
+    result = duckdb.sql(sql_query).df()
+    st.dataframe(result)
+
+with tab1:
+    st.write("Chats")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+    st.write("Chien")
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+    st.write("Hibou")
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
